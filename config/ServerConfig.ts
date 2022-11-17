2de9d187bf9396d79f0e02d4ec7391d2e628f777
jkkang <jkkang@smartauth.kr>
2022-11-17 14:30:38 +0900

    add feature to export validator list to CSV

--- config/ServerConfig.ts
+++ config/ServerConfig.ts
@@ -1,8 +1,7 @@
-import { Platform } from 'react-native';
-
 export const httpLinkURI = process.env.SERVER_URL;
 export const webSocketURI = process.env.WEBSOCKET_URL;
 export const serverChainID = process.env.CHAIN_ID;
 export const httpServerURI = process.env.SERVER_URL;
 export const openDappURI = process.env.OPENDAPP_URL;
-export const proposalInfoURI = process.env.PROPOSALINFO_URL;
+export const proposalInfoURI = `${process.env.EXPORT_URL || ''}/proposal`;
+export const exportValidatorURI = `${process.env.EXPORT_URL || ''}/validator`;
